bbc593024ef291071d6f197e84a6249972a9b4b2
ANAND SELVAM <anandselvam696@gmail.com>
2024-07-25 03:03:49 +0000

    AnandSevlam

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/Nexus-ZKvm-2/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspace/Nexus-ZKvm-2/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspace/Nexus-ZKvm-2/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspace/Nexus-ZKvm-2/nexus-project/src/main.rs
